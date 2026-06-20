3f732b84d1f1ddd7220a444efe34e18753d14afa
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2026-06-20 20:30:44 +0200

    feat: add Shopify Storefront API client and env config

--- src/lib/shopify.ts
+++ src/lib/shopify.ts
@@ -3,5 +3,5 @@ import { createStorefrontApiClient } from "@shopify/storefront-api-client";
 export const shopify = createStorefrontApiClient({
     storeDomain : process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN!,
     apiVersion : '2026-04',
-    publicAccessToken : process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN!,
+     publicAccessToken : process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_TOKEN!,
 });
